13ee3579c520b6d8359a525c6273abb09d9c3e04
megane <megane.solliard@students.hevs.ch>
2022-04-26 14:31:36 +0200

    Ajoutez des fichiers projet.

---
+++ VSFly/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VSFly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
